78543a9a4c446707707c552d86e0578cbc746128
Vihanga <58096787+deshanviha@users.noreply.github.com>
2020-09-20 10:08:19 +0530

    vihanga commited

---
+++ app/src/main/java/com/example/shoppingapp/addCard.java
@@ -0,0 +1,17 @@
+package com.example.shoppingapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class addCard extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_card_details);
+
+    }
+}

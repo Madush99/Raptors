98578b1edeeee926ca00a6da9548c84ddc6f745d
Vihanga <58096787+deshanviha@users.noreply.github.com>
2020-09-20 13:34:41 +0530

    vihanga commited

---
+++ app/src/main/java/com/example/shoppingapp/payment_report.java
@@ -0,0 +1,15 @@
+package com.example.shoppingapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class payment_report extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.confirm_payment_app);
+
+    }
+}
